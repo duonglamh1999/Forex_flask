6a21d49b00416e8daf31ea203abc5ac79d59c78e
duonglamh1999 <duonglamh1999@gmail.com>
2022-08-08 18:13:54 -0500

    Inital

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, redirect, request, render_template,session
+from asyncio.windows_events import NULL
+from flask import Flask, redirect, request, render_template,session,flash
 from flask_debugtoolbar import DebugToolbarExtension
-
+from converter import convert,check
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 RESPONSES_KEY = "responses"
+
 @app.route("/")
 def start():
-    return render_template('index.html')
+    value=None
+    return render_template('form.html')
+
+@app.route("/convert",methods=["POST"])
+def handle_response():
+    cur1 = request.form.get("Current1")
+    cur2 = request.form.get("Current2")
+    amount = request.form.get("Amount")
+    checking = check([cur1,cur2],amount)
+    if len(checking)>0:
+        for message in checking:
+            flash(message)
+        return redirect('/')
+    global value
+    value=convert(cur1,cur2,amount)
+    return redirect('/answer')
+
+@app.route("/answer")
+def answer():
+    return render_template('answer.html',value= value)
--- converter.py
+++ converter.py
@@ -1,19 +1,44 @@
 from decimal import Decimal
 from forex_python.converter import CurrencyRates,CurrencyCodes
-
-def convert(cur1,cur2,amount):
-    code = getCode(cur2)
-    rate = getRate(cur1,cur2,amount)
-    return f'{code} {rate:.2f}'
-
+#single functionality function
+def checkCur(cur):
+        c = CurrencyRates() 
+        if len(cur)<1:return 'Empty currency input'
+        try:
+                c.get_rates(cur) 
+                return True
+        except:
+                return f'Invalid currency:{cur}'
+def checkAmount(amount):
+        if len(amount)<1:return 'Empty amount input'
+        try:
+                amtType = float(amount)
+                return True if amtType >=0 else f'Negative amount: {amount}'
+        except:
+                return f'Invalid input: {amount}'
 def getRate (cur1,cur2,amount):
-    r = CurrencyRates()
-    rate= r.convert(cur1,cur2,Decimal(amount))
-    return rate
-    
+        r = CurrencyRates()
+        rate= r.convert(cur1,cur2,Decimal(amount))
+        return rate
 
 def getCode(curCode):
-    c = CurrencyCodes()
-    code = c.get_symbol(curCode)
-    return code
-print(convert('USD','EUR',200))
+        c = CurrencyCodes()
+        code = c.get_symbol(curCode)
+        return code
+
+#combine functions
+
+def convert(cur1,cur2,amount):
+        rate = getRate(cur1,cur2,amount)
+        code = getCode(cur2)
+        return f'{code} {rate:.2f}'
+
+def check(arr,amount):
+        message =[]
+        for cur in arr:
+                if checkCur(cur) is not True:
+                        message.append(checkCur(cur))
+        if checkAmount(amount) is not True:
+                message.append(checkAmount(amount))
+        return message
+
